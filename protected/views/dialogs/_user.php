25a12ea51e16a37b01b47ea743ee639a8efc6f71
Ilya I. Vorozhbit <ilya@mctop.im>
2013-11-25 22:50:35 +0400

    oups

--- protected/views/dialogs/_user.php
+++ protected/views/dialogs/_user.php
@@ -1,6 +1,6 @@
 <?php
 
-    $messages_count = Messages::model()->count('recipient='.Yii::app()->user->id.' and dialog = '.$dialog->id.'');
+    $messages_count = Messages::model()->count('recipient='.Yii::app()->user->id.' and dialog = '.$dialog->id.' and status=0');
 
     if($messages_count>0)
         $dialogs_label = ' ('.$messages_count.')';
