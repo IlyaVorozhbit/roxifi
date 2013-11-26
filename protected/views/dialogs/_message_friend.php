8a7d47f585a1355769cd38b6339440ed68753b40
Ilya I. Vorozhbit <ilya@mctop.im>
2013-11-27 00:17:54 +0400

    styles

--- protected/views/dialogs/_message_friend.php
+++ protected/views/dialogs/_message_friend.php
@@ -1,6 +1,6 @@
 <div class="friend">
     <?php if ($message->status==0) echo '<span class="label">new</span>';?>
-    <?php echo $message->time;?> <a href="/u<?php echo $author->id;?>"><?php echo $author->login;?></a><br>
+    <a href="/u<?php echo $author->id;?>"><?php echo $author->login;?></a> <?php echo $message->time;?> <br>
     <?php echo $message->text;?>
     <div align="right">
         <?php /*<a href="/dialogs/deletemessage/<?php echo $message->id;?>"><i class="icon-trash"></i></a>*/?>
