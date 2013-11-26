f25a6b4f7a1947c9a21ea176ac9daa57769113c9
Ilya I. Vorozhbit <ilya@mctop.im>
2013-11-26 20:48:16 +0400

    Edit page fix translations

--- protected/views/users/Edit.php
+++ protected/views/users/Edit.php
@@ -1,7 +1,7 @@
 <?php
   $lang = new Language;
-  echo $lang->Translate(36).'<hr>';
-  echo $lang->Translate(35).':'; ?>
+  echo Yii::t('profile', 'Profile info').'<hr>';
+  echo Yii::t('profile', 'Image').':'; ?>
   <div style= "border: 1px solid #f3f3f3; height: 300px; width: 300px; background: url('<?php $avatar = UsersImages::model()->find('user = :user', array('user'=>$user->id));
                                                                                  echo $avatar !== NULL ? '/avatars/u'.$user->id.'/'.$avatar->filename :
                                                                                   '/images/no_avatar.png'; ?>') no-repeat center;"></div>
@@ -14,8 +14,8 @@
   echo '<table><tr><td>';
   echo $form->fileField(UsersImages::model(), 'filename');
   echo '</td></tr><tr><td>';
-  echo CHtml::submitButton($lang->Translate(37), array('style'=>'margin: 0px;'));
-  echo $avatar !== NULL ? '<a href="/u'.Yii::app()->user->id.'/edit?delete_image">'.CHtml::button($lang->Translate(38), array('style'=>'margin-left: 2px;')).'</a>' : '';
+  echo CHtml::submitButton(Yii::t('profile', 'Apply'), array('style'=>'margin: 0px;'));
+  echo $avatar !== NULL ? '<a href="/u'.Yii::app()->user->id.'/edit?delete_image">'.CHtml::button(Yii::t('profile', 'Delete'), array('style'=>'margin-left: 2px;')).'</a>' : '';
   echo '</td></tr></table>';
   $this->endWidget();
   $form = $this->beginWidget('CActiveForm', array(
@@ -24,12 +24,12 @@
     'htmlOptions'=>array('enctype'=>'multipart/form-data'),
   ));
   echo '<table><tr><td>';
-  echo $lang->Translate(2).':';
+  echo Yii::t('profile', 'Language').':';
   echo '</td></tr><tr><td>';
   echo '<select name="Users[language]">'.($user->language == 'en' ? '<option selected value="en">English</option><option value="ru">Русский</option>' :
                                              '<option value="en">English</option><option selected value="ru">Русский</option>').'</select>';
   echo '</td></tr><tr><td>';
-  echo CHtml::submitButton($lang->Translate(37), array('style'=>'margin: 0px;'));
+  echo CHtml::submitButton(Yii::t('profile', 'Apply'), array('style'=>'margin: 0px;'));
   echo '</td></tr></table>';
   $this->endWidget();
 ?>
