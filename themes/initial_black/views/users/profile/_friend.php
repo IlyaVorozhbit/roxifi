9942552ba143ec657d5f26b3d03f1e26e39ff469
Ilya I. Vorozhbit <ilya@mctop.im>
2013-12-06 00:09:59 +0400

    styles

--- themes/initial_black/views/users/profile/_friend.php
+++ themes/initial_black/views/users/profile/_friend.php
@@ -4,6 +4,6 @@
         $profile_image =  $avatar !== NULL ? '/avatars/u'.$friend->id.'/'.$avatar->filename : '/images/no_avatar.png';
         $fullname = Users::model()->getFullName($friend->id);
     ?>
-    <a href='/u<?php echo $friend->id;?>'><img style="width: 100px; height: 100px; border: 1px solid #E7E7E7;" src='<?php echo $profile_image;?>'></a>
+    <a href='/u<?php echo $friend->id;?>'><img style="width: 70px; height: 70px; border: 1px solid #E7E7E7;" src='<?php echo $profile_image;?>'></a>
     <div class="name"><?php echo $fullname['name'].' '.$fullname['surname']?></div>
 </div>
